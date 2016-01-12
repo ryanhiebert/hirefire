719838250a2b8e10e3f98153b22f71318084cc0c
Ryan Hiebert <ryan@ryanhiebert.com>
2016-01-11 23:37:53 -0600

    Use inspect to get active jobs from workers
    In-progress jobs haven't been considered so far in HireFire. This adds
    getting those active jobs. Unfortunately, the methods to run to get
    those jobs are quite expensive, so use the cache to make sure that each
    method is only run one time.

--- hirefire/procs/celery.py
+++ hirefire/procs/celery.py
@@ -1,8 +1,60 @@
 from __future__ import absolute_import
+from itertools import chain
 
 from celery.app import app_or_default
 
+from ..utils import KeyDefaultDict
 from . import Proc
+
+
+class CeleryInspector(KeyDefaultDict):
+    """
+    A defaultdict that manages the celery inspector cache.
+    """
+
+    def __init__(self, app):
+        super(CeleryInspector, self).__init__(self.get_status_task_counts)
+        self.app = app
+        self.route_queues = None
+
+    def get_route_queues(self):
+        """Find the queue to each active routing pair.
+
+        Cache to avoid additional calls to inspect().
+
+        Returns a mapping from (exchange, routing_key) to queue_name.
+        """
+        if self.route_queues is not None:
+            return self.route_queues
+
+        worker_queues = self.app.control.inspect().active_queues()
+        active_queues = chain.from_iterable(worker_queues.values())
+
+        self.route_queues = {
+            (queue['exchange']['name'], queue['routing_key']): queue['name']
+            for queue in active_queues
+        }
+        return self.route_queues
+
+    def get_status_task_counts(self, status):
+        """Get the tasks on all queues for the given status.
+
+        This is called lazily to avoid running long methods when not needed.
+        """
+        if status not in ['active', 'reserved', 'scheduled']:
+            raise KeyError('Invalid task status: {}'.format(status))
+
+        route_queues = self.get_route_queues()
+        inspected = getattr(self.app.control.inspect(), status)()
+
+        counts = KeyDefaultDict(lambda: 0)
+        for task in chain.from_iterable(inspected.values()):
+            exchange = task['delivery_info']['exchange']
+            routing_key = task['delivery_info']['routing_key']
+            queue = route_queues[exchange, routing_key]
+            counts[queue] += 1
+
+        return counts
 
 
 class CeleryProc(Proc):
@@ -52,6 +104,10 @@ class CeleryProc(Proc):
     #: The Celery app to check for the queues (optional).
     app = None
 
+    #: The Celery task status to check for on workers (optional).
+    #: Valid options are 'active', 'reserved', and 'scheduled'.
+    inspect_statuses = []  # Empty to default to previous results
+
     def __init__(self, app=None, *args, **kwargs):
         super(CeleryProc, self).__init__(*args, **kwargs)
         if app is not None:
@@ -60,7 +116,7 @@ class CeleryProc(Proc):
         self.connection = self.app.connection()
         self.channel = self.connection.channel()
 
-    def quantity(self, **kwargs):
+    def quantity(self, cache=None, **kwargs):
         """
         Returns the aggregated number of tasks of the proc queues.
         """
@@ -81,4 +137,17 @@ class CeleryProc(Proc):
                 pass
             else:
                 count += queue.message_count
+
+        if cache is not None and self.inspect_statuses:
+            count += self.inspect_count(cache)
+
         return count
+
+    def inspect_count(self, cache):
+        """Use Celery's inspect() methods to see tasks on workers."""
+        cache.setdefault('celery_inspect', KeyDefaultDict(CeleryInspector))
+        return sum(
+            cache['celery_inspect'][self.app][status][queue]
+            for status in self.inspect_statuses
+            for queue in self.queues
+        )
